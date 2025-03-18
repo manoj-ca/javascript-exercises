0dedf5be527b25914822747879e947d22741cf01
Manoj Prasad <manoj_ca@yahoo.com>
2025-03-18 12:58:09 -0700

    repeat string solution

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(text, count) {
+  if (count < 0) return 'ERROR';
+  let out = '';
+  for (let i = 0; i < count; i++) {
+    out += text;
+  }
+  return out;
 };
 
 // Do not edit below this line
