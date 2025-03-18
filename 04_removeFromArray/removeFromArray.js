a336402cc10f8583537c5b2994df82f62c3ab627
Manoj Prasad <manoj_ca@yahoo.com>
2025-03-18 15:03:49 -0700

    remove from array solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(list) {
+  if (arguments.length > 1) {
+    for (let i = 1; i < arguments.length; i++) {
+      list = list.filter(item => item !== arguments[i]);
+    }
+  }
+  return list;
 };
 
 // Do not edit below this line
