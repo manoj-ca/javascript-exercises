faeae3af66a1f86f504bb142c1aa89f4fe1c76e8
Manoj Prasad <manoj_ca@yahoo.com>
2025-03-29 12:51:44 -0700

    fibonacci solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(a) {
+  if (a < 0) return 'OOPS';
+  if (a == 0) return 0;
+  let first = 1;
+  let second = 1;
+  let next = 2;
+  if (a < 3) return 1;
+  for (let i = 2; i < a; i++) {
+    next = first + second;
+    first = second;
+    second = next;
+  }
+  return next;
 };
 
 // Do not edit below this line
