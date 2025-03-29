f20450d4e2ddb3600f3cd8daa17939a00d5f08c7
Manoj Prasad <manoj_ca@yahoo.com>
2025-03-29 12:26:14 -0700

    palindromes solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  str = str.replace(/[!.,]/g, '').toLowerCase().split(" ").join("");
+  const res = str.split('').slice().reverse().join('');
+  return res == str;
 };
 
 // Do not edit below this line
